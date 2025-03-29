943f1a735658aef4090278e27ceed19cb5b3ff54
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2025-03-29 23:38:39 +0000

    Update pipe.c

--- class_codes/lec3/pipe.c
+++ class_codes/lec3/pipe.c
@@ -7,26 +7,35 @@
 int main(int argc, char *argv[]) {
     int pipefd[2];
 
-    if (pipe(pipefd) == -1) {
+    int rc = pipe(pipefd);
+
+    if (rc < 0) {
         perror("pipe");
         exit(1);
     }
 
-    int rc = fork();
+    rc = fork();
+
     if (rc < 0) {
         perror("fork");
         exit(1);
     } else if (rc == 0) {
         // CHILD PROCESS
-        close(pipefd[0]); // close unused read end
-        dup2(pipefd[1], STDOUT_FILENO); // redirect stdout to pipe
-        close(pipefd[1]); // safe to close after dup2
+
+        // close unused read end
+        close(pipefd[0]); 
+
+        // redirect stdout to pipe
+        dup2(pipefd[1], STDOUT_FILENO); 
+        
+        // close write end as it is not needed anymore 
+        //(if needed don't close)
+        close(pipefd[1]); 
 
         char *myargs[3];
         myargs[0] = strdup("wc");
         myargs[1] = strdup("text1.txt");
         myargs[2] = NULL;
-
         execvp(myargs[0], myargs);
 
         //these line should not execute
@@ -35,22 +44,27 @@ int main(int argc, char *argv[]) {
         exit(1);
     } else {
         // PARENT PROCESS
-        close(pipefd[1]); // close unused write end
+
+         // close unused write end
+        close(pipefd[1]);
+
         wait(NULL); // wait for child to finish
 
+        // read from the read end
         char buffer[128];
         int n = read(pipefd[0], buffer, sizeof(buffer) - 1);
         if (n > 0) {
             buffer[n] = '\0';
             int lines, words, chars;
+            //parse data delimited by spaces
             sscanf(buffer, "%d %d %d", &lines, &words, &chars);
-
             printf("Data received from the child process indicates there are %d new line characters, %d characters, and %d words in the file.\n",
                    lines, chars, words);
         } else {
             printf("No data received from child.\n");
         }
 
+        //close the read end of the pipe
         close(pipefd[0]);
         printf("Parent done.\n");
     }
