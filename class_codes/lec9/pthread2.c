87b75af6d2bb7114e85c899a2e4e91763d2eb968
sarimbaig01 <87637747+sarimbaig01@users.noreply.github.com>
2025-05-05 21:40:36 +0100

    Update pthread2.c

--- class_codes/lec9/pthread2.c
+++ class_codes/lec9/pthread2.c
@@ -1,1 +1,28 @@
+#include <pthread.h>
+#include <stdio.h>
 
+int g_count = 0;
+
+void* counter()
+{
+    for(int i = 0; i < 10000; i++){
+        g_count++;
+    }
+    
+    return NULL;
+}
+
+int main(int argc, char* argv[]){
+
+    pthread_t t1, t2;
+
+    pthread_create(&t1, NULL, counter, NULL);
+    pthread_create(&t2, NULL, counter, NULL);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+
+    printf("Final value of g_count: %d\n", g_count);
+
+    return 0;
+}
